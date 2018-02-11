07e0fcd353483c60939392aec1a8d677023a6dd4
nlaurance <nicolas.laurance@gmail.com>
2018-02-11 16:11:09 +0000

    now it works

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,55 @@
 #include <Arduino.h>
 #include <RH_ASK.h>
-#include <SPI.h>
+#include <SPI.h> // Not actualy used but needed to compile
 
 RH_ASK driver;
 
+// direction
+const int NO_ORDER = 0;
+const int FORWARD = 1;
+const int BACKWARD = 2;
+const int TURN_RIGHT = 4;
+const int TURN_LEFT = 8;
 
-const int xPin = A1;
-const int yPin = A0;
-const int buttonPin = 12;
-
-// propulsion
-const int motor1 = 6;
-const int motor2 = 7;
-const int PWMPropulsionPin = 5;
-
-
-void setup() {
-  Serial.begin(9600);
+void setup()
+{
+  Serial.begin(9600); // Debugging only
+  Serial.println("Setup");
+  if (!driver.init())
+    Serial.println("init failed");
 }
 
-void loop(){
-  uint8_t buf[12];
-  uint8_t buflen = sizeof(buf);
-  if (driver.recv(buf, &buflen)) // Non-blocking
+void loop() {
+  //uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+  int orders[4];
+  uint8_t buflen = sizeof(orders);
+
+  //if (driver.recv(buf, &buflen)) // Non-blocking
+  if (driver.recv((uint8_t*)orders, &buflen)) //if data is not an array, use &receivedData
   {
-    Serial.print("Message: ");
-    Serial.println((char*)buf);
-    // Message with a good checksum received, dump it.
-  // if (buf == "avant") {
-  //   digitalWrite(motor1, LOW);
-  //   digitalWrite(motor2, HIGH);
-  // }
+
+    for (byte i = 0; i < 4; i++)
+    {
+      Serial.println(orders[i]);
+    }
+
+    int propulsion;
+    int vitesse;
+    int direction;
+    int braquage;
+
+    propulsion=orders[0];
+    vitesse=orders[1];
+    direction=orders[2];
+    braquage=orders[3];
+
+    if (propulsion == FORWARD) {
+      // do something
+    }
+  Serial.println("====RECEIVED=========");
+  }
+  else {
+    Serial.println("====NO MESS=========");
 }
+  delay(2000);
 }
